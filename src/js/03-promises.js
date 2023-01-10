563daf77cdcbf73b27cc2826fb33aa9613b9f217
EllaHonchar <kuchincus82@gmail.com>
2023-01-10 23:34:29 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const { elements: { delay, step, amount }} = form;
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setInterval(() => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay })
+    } else {
+      reject({ position, delay })
+    }
+  }, delay);
+  })
+  return promise;
 }
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  for (let i = 1, delayTime = Number(delay.value);
+    i <= Number(amount.value);
+    i += 1, delayTime += Number(step.value)
+  ) {
+    createPromise(i, delayTime).then(onSuccess).catch(onError);
+  }
+};
+
+function onSuccess ({ position, delay }) {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  };
+function onError ({ position, delay }) {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  };
